f898bdadde0e686b7aabb825ac62fe015ec3c229
Miran <miranabdelaziz@yahoo.com>
2024-09-10 23:58:20 -0400

    -created ExerciseCard  component

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
-      <Workout />
+      <Main />
       <Generator
         poison={poison}
         setPoison={setPoison}
