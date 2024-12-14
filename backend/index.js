979bd3ad09e0bada636adaedcf236ff9df760298
Miran <miranabdelaziz@yahoo.com>
2024-12-13 22:59:42 -0500

    - Configured PostgreSQL connection using `pg` package - Added `database credentials in .env - Verified successful database connection usting test.js

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Get Fit API is running');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ backend/test.js
@@ -0,0 +1,15 @@
+const pool = require("./db");
+
+const testConnection = async () => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        console.log("Database connected successfully!");
+        console.log("Current time from database:", result.rows[0]);
+    } catch (err) {
+        console.error("Database connection error:", err);
+    } finally {
+        pool.end(); // Close the connection pool
+    }
+};
+
+testConnection();
