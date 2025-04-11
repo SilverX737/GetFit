309dbccf59c877d5248de95d1f30b40964d434eb
Miran <miranabdelaziz@yahoo.com>
2025-04-10 23:56:10 -0400

    -moved folder structures arround

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { PrismaClient } = require("@prisma/client");
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
     res.send('Get Fit API is running');
--- backend/test.js
+++ backend/test.js
@@ -1,4 +1,4 @@
-const pool = require("./db");
+const pool = require("./src/db");
 
 const testConnection = async () => {
     try {
